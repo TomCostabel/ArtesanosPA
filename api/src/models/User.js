8e9ffd10a462649c48f8646b6f8ec61f63710c38
TomCostabel <tomasperalta1997@hotmail.com>
2023-04-19 10:40:18 -0300

    Update Models User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -19,6 +19,11 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     token: { type: String, default: null },
+    provincia: { type: String, default: null },
+    ciudad: { type: String, default: null },
+    codigoPostal: { type: String, default: null },
+    direccion: { type: String, default: null },
+    dni: { type: String, default: null },
 });
 
 module.exports = mongoose.model("User", UserSchema);
