610f2deaf31737bc17300491ed223443afa4a57d
TomCostabel <tomasperalta1997@hotmail.com>
2023-05-02 15:43:26 -0300

    categoria css

--- src/Components/Catalogo/Catalogo.jsx
+++ src/Components/Catalogo/Catalogo.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import Loading from "../Loading/Loading";
 import "../Catalogo/Catalogo.css";
-import img from "../../assets/img/artesanosLogo.jpg";
+import imgCatalogo from "../../assets/img/catalogo.png";
 import { Link } from "react-router-dom";
 // import Productos from "../../productos.json";
 
@@ -19,53 +19,57 @@ export default function Catalogo() {
             ) : (
                 <div>
                     <NavBar />
-                    <div className="container-catalogo">
-                        <div className="container-centrado-catalogo">
-                            <Link to="/Catalogo/DijesEnBrutoConAlpaca">
-                                <div className="card-individual-catalogo">
-                                    <img className="img-catalogo" src={img} />
+                    <div className="container-principal">
+                        <img
+                            className="img-catalogo-inicio"
+                            src={imgCatalogo}
+                        />
+                        <div className="container-principal-categoria">
+                            {/* <Link to="/Catalogo/DijesEnBrutoConAlpaca"> */}
+                            <div className="card-individual-catalogo">
+                                {/* <img className="img-catalogo" src={img} /> */}
 
-                                    <h3 className="titulo-catalogo">
-                                        DIJES EN BRUTO
-                                        <br /> CON ALPACA
-                                    </h3>
-                                </div>
-                            </Link>
-                            <Link to="/Catalogo/DijesEnBruto">
-                                <div className="card-individual-catalogo">
-                                    <img className="img-catalogo" src={img} />
+                                <h3 className="titulo-catalogo">
+                                    DIJES EN BRUTO CON ALPACA
+                                </h3>
+                            </div>
+                            {/* </Link> */}
+                            {/* <Link to="/Catalogo/DijesEnBruto"> */}
+                            <div className="card-individual-catalogo">
+                                {/* <img className="img-catalogo" src={img} /> */}
 
-                                    <h3 className="titulo-catalogo">
-                                        DIJES EN BRUTO
-                                    </h3>
-                                </div>
-                            </Link>
-                            <Link to="/Catalogo/DijesRolados">
-                                <div className="card-individual-catalogo">
-                                    <img className="img-catalogo" src={img} />
+                                <h3 className="titulo-catalogo">
+                                    DIJES EN BRUTO
+                                </h3>
+                            </div>
+                            {/* </Link> */}
+                            {/* <Link to="/Catalogo/DijesRolados"> */}
+                            <div className="card-individual-catalogo">
+                                {/* <img className="img-catalogo" src={img} /> */}
 
-                                    <h3 className="titulo-catalogo">
-                                        DIJES ROLADOS
-                                    </h3>
-                                </div>
-                            </Link>
-                            <Link to="/Catalogo/DijesImportados">
-                                <div className="card-individual-catalogo">
-                                    <img className="img-catalogo" src={img} />
+                                <h3 className="titulo-catalogo">
+                                    DIJES ROLADOS
+                                </h3>
+                            </div>
+                            {/* </Link> */}
 
-                                    <h3 className="titulo-catalogo">
-                                        DIJES IMPORTADOS
-                                    </h3>
-                                </div>
-                            </Link>
-                            <Link to="/Catalogo/DijesConFormas">
-                                <div className="card-individual-catalogo">
-                                    <img className="img-catalogo" src={img} />
-                                    <h3 className="titulo-catalogo">
-                                        DIJES CON FORMAS
-                                    </h3>
-                                </div>
-                            </Link>
+                            {/* <Link to="/Catalogo/DijesImportados"> */}
+                            <div className="card-individual-catalogo">
+                                {/* <img className="img-catalogo" src={img} /> */}
+
+                                <h3 className="titulo-catalogo">
+                                    DIJES IMPORTADOS
+                                </h3>
+                            </div>
+                            {/* </Link> */}
+                            {/* <Link to="/Catalogo/DijesConFormas"> */}
+                            <div className="card-individual-catalogo">
+                                {/* <img className="img-catalogo" src={img} /> */}
+                                <h3 className="titulo-catalogo">
+                                    DIJES CON FORMAS
+                                </h3>
+                            </div>
+                            {/* </Link> */}
                         </div>
                     </div>
                 </div>
