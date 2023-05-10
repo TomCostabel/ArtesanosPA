32f75839ad0dcf55c74af677789a74e45754e72d
TomCostabel <tomasperalta1997@hotmail.com>
2023-05-10 15:40:21 -0300

    Logica de envios

--- src/Components/Envios/Envios.jsx
+++ src/Components/Envios/Envios.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import "../Envios/Envios.css";
+import Loading from "../Loading/Loading";
 export default function () {
+    const [loading, setLoading] = useState(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 1200);
     return (
-        <div>
-            <NavBar />
-            <div className="container-centrado-envios">
-                <h2>Retirar por el comercio (Punta Alta)</h2>
-                <h2>Envio a domicilio otra ciudad</h2>
-                <h2>Envio al correo otra ciudad</h2>
-            </div>
-        </div>
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <div>
+                    <NavBar />
+                    <div className="container-centrado-envios">
+                        <h2>Retirar por el comercio (Punta Alta)</h2>
+                        <h2>Envio a domicilio otra ciudad</h2>
+                        <h2>Envio al correo otra ciudad</h2>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
